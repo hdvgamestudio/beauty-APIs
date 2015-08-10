a3db7856bdefe343e842e774615d4e26d23c0cbf
ngocnv <nguyenngoc101@yahoo.com>
2015-08-10 12:05:12 +0700

    add config files

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  secretKey: 'this is a secret key, right here',
+  server: {
+  name: 'beauty-APIs',
+  version: '1.0.0',
+  port: 3000
+  },
+  database: {
+  host: 'mongodb://localhost',
+  port: '27017',
+  dbname: 'beauty'
+  }
+}
