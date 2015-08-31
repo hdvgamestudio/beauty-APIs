ac9a817768bbdf9dcaa3da3012b05f4680e27f21
lntitbk <lnt.itbk@gmail.com>
2015-08-31 11:11:20 +0700

    fix tag.js

--- app/controllers/tag.js
+++ app/controllers/tag.js
@@ -1,24 +1,12 @@
-//body
-/*
-{
-	tag: {
-		"name": "nodejs";
-	}
-}
-*/
 var Error400 = require('../../lib/errors/error400');
 var ApiErrors = require('../../lib/apiError');
 var Tag = require('../models/tag');
-
-var isValid;
-
 exports.getTags = function( req, res, next){
 	Tag.find( function(err, users) {
 		if(err)
-			res.send(err);
-		else res.send(users);
+			res.json(err);
+		else res.json(users);
 	});
-	console.log('hello');
 }
 
 // insert new tag
@@ -30,7 +18,7 @@ exports.postTags = function( req, res, next) {
 		));
 	Tag.findOne({ name : tagName },
 		function(err, tag){
-			if(err) return next(new Error(err.message));
+			if(err) return next(err);
 			if(tag) return next(new Error400(
 				ApiErrors.TAG_NAME_EXISTED.code,
 				ApiErrors.TAG_NAME_EXISTED.msg
@@ -38,7 +26,7 @@ exports.postTags = function( req, res, next) {
 			// To do save
 			var newTag = new Tag({ name: tagName });
 			newTag.save( function(err, savedTag){
-				if(err) return next(new Error(err.message));
+				if(err) return next(err);
 				res.json(savedTag);
 			});
 		});
