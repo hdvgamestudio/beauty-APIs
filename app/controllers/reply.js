2a395b96c89da47874458a180e952ff36d5fcf0d
ngocnv <nguyenngoc101@yahoo.com>
2015-08-31 16:19:59 +0700

    create reply controller

---
+++ app/controllers/reply.js
@@ -0,0 +1,15 @@
+var Comment     = require('../models/comment')
+
+exports.postReplies = function(req, res, next) {
+  var comment_id = req.params.comment_id;
+}
+
+exports.getReplies = function(req, res, next) {
+
+}
+exports.editReplies = function(req, res, next) {
+
+}
+exports.deleteReplies = function(req, res, next) {
+
+}
