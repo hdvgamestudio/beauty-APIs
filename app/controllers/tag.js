e4e7c3596502798db413730b0e8939aa8bda4eb3
lntitbk <lnt.itbk@gmail.com>
2015-08-31 15:15:34 +0700

    fix tag.js

--- app/controllers/tag.js
+++ app/controllers/tag.js
@@ -1,32 +1,37 @@
 var Error400 = require('../../lib/errors/error400');
 var ApiErrors = require('../../lib/apiError');
 var Tag = require('../models/tag');
-exports.getTags = function( req, res, next){
-	Tag.find( function(err, users) {
-		if(err)
-			res.json(err);
+
+
+exports.getTags = function(req, res, next) {
+  var offset = parseInt(req.query.offset);
+  var limit = parseInt(req.query.limit);
+	Tag.find(function(err, users) {
+		if(err) res.json(err);
 		else res.json(users);
-	});
+	})
+  .skip(offset)
+  .limit(limit);
 }
 
 // insert new tag
 exports.postTags = function( req, res, next) {
 	var tagName = req.body.tag.name;
-  if( !tagName ) return next(new Error400(
+  if (!tagName) return next(new Error400(
 		ApiErrors.TAG_IS_NULL.code,
 		ApiErrors.TAG_IS_NULL.msg
-		));
+	));
 	Tag.findOne({ name : tagName },
 		function(err, tag){
-			if(err) return next(err);
-			if(tag) return next(new Error400(
+			if (err) return next(err);
+			if (tag) return next(new Error400(
 				ApiErrors.TAG_NAME_EXISTED.code,
 				ApiErrors.TAG_NAME_EXISTED.msg
 			));
 			// To do save
 			var newTag = new Tag({ name: tagName });
 			newTag.save( function(err, savedTag){
-				if(err) return next(err);
+				if (err) return next(err);
 				res.json(savedTag);
 			});
 		});
