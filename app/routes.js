a80127ac07c52527f406ba3e7e73507c9d923ac6
lntitbk <lnt.itbk@gmail.com>
2015-09-07 14:59:01 +0700

    fix router

--- app/routes.js
+++ app/routes.js
@@ -96,7 +96,6 @@ module.exports = function(app) {
   router.route('/shops')
     .get(shopController.getShops)
     .post(validateBody, shopController.postShops);
-
   router.route('/shops/:id')
     .put(validateBody,shopController.editShops)
     .delete(shopController.deleteShops);
@@ -105,7 +104,6 @@ module.exports = function(app) {
  router.route('/distributors')
    .get(distributorController.getDistributors)
    .post(validateBody, distributorController.postDistributors);
-
  router.route('/distributors/:id')
    .put(validateBody, distributorController.editDistributors)
    .delete(distributorController.deleteDistributors);
